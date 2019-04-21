bd1addc693bfb571c87613bfa52e97b06ad79f95
Alon Tzarafi <alontzarafi@gmail.com>
2019-04-21 16:55:25 +0300

    Rename 'battle-haxe-mode'

--- battle-haxe.el
+++ battle-haxe.el
@@ -26,7 +26,7 @@
 ;; This package offers a development system for the Haxe programming language.
 ;; Haxe code completion is activated using the `company-mode' package.
 ;; Options like "go to definition" and "find all references" are available, as well as `eldoc' support.
-;; All of those features are triggered in `battle-haxe-services-mode' which also spawns a Haxe server to perform them.
+;; All of those features are triggered in `battle-haxe-mode' which also spawns a Haxe server to perform them.
 ;; The tools rely on the Haxe "compiler services" feature ( https://haxe.org/manual/cr-completion-overview.html ).
 ;; The main quirk is that the system has to force automatic saving of the edited Haxe buffer.
 ;; If this is a problem for you don't use the package.
@@ -94,17 +94,17 @@ Used to determine if a new call to Haxe compiler services is needed.")
 (defconst battle-haxe-non-symbol-chars "[^[:alnum:]?_]"
   "Characters disallowed in Haxe symbol names.")
 
-(defvar battle-haxe-services-mode-map
+(defvar battle-haxe-mode-map
   (let ((map (make-sparse-keymap)))
     map)
-  "Keymap used for symbol `battle-haxe-services-mode'.")
+  "Keymap used for symbol `battle-haxe-mode'.")
 
 ;;;###autoload
-(define-minor-mode battle-haxe-services-mode
+(define-minor-mode battle-haxe-mode
   "Haxe completion, and other coding tools, using Haxe's compiler services."
   :lighter " haxe"
   :global nil
-  :keymap battle-haxe-services-mode-map
+  :keymap battle-haxe-mode-map
   ;;Init the minor mode
   (progn
     ;; Setup company for Haxe, and start server if needed
@@ -124,7 +124,7 @@ Used to determine if a new call to Haxe compiler services is needed.")
     
     (prefix
      (if
-         (and (bound-and-true-p battle-haxe-services-mode)
+         (and (bound-and-true-p battle-haxe-mode)
               (not (company-in-string-or-comment)))
          (let ((prefix (battle-haxe-get-company-prefix)))
            (if (looking-back
